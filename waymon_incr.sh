33c3c8dd35f0d3bdd34fd7a05379fae777e9a668
wakoo <wakoo1337@yandex.ru>
2024-07-14 17:12:29 +0300

    Сделан вариант под стационарный компьютер

--- waymon_incr.sh
+++ waymon_incr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-MODULO=5
+MODULO=4
 echo $(((`cat ~/.waymon_num 2>/dev/null` + 1) % $MODULO )) > ~/.waymon_num
--- waymon_show.sh
+++ waymon_show.sh
@@ -7,7 +7,6 @@ case $VALUE in
 	0 ) grep cpu /proc/stat | tail -1 | awk '{print "ЦП: " 100-(($5*100)/($2+$3+$4+$5+$6+$7+$8+$9+$10)) " %"}' ;;
 	1 ) grep cpu\ MHz /proc/cpuinfo | awk ' BEGIN { sum_freq=0; cores=0 } {(cores++); (sum_freq += $(NF)) } END {print (sum_freq / cores) " МГц"}' ;;
 	2 ) awk 'BEGIN { total = 0;avail=0 } { if ($1 == "MemTotal:") total=$2 ; if ($1 == "MemAvailable:") avail=$2 } END {print "ОЗУ: " (((total-avail) >= 1048576) ? ((total-avail)/1048576 " ГБ") : ((total-avail)/1024 " МБ")) } ' /proc/meminfo ;;
-	3 ) cat /sys/class/thermal/thermal_zone5/temp | awk '{print ($1/1000) " °C" }' ;;
-	4 ) paste /sys/class/power_supply/BAT0/capacity /sys/class/power_supply/BAT0/energy_now /sys/class/power_supply/BAT0/power_now | awk '{print "Батарея: " ($1) " %" ($3 != 0?" " ($2/$3) " ч":"") }' ;;
+	3 ) cat /sys/class/thermal/thermal_zone0/temp | awk '{print ($1/1000) " °C" }' ;;
 esac
 
